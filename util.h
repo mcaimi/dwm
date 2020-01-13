bd28b60c578a1f9e36915f663f4ea10088e3a174
Marco Caimi <caimi.marco@protonmail.ch>
2020-01-13 11:28:51 +0100

    util: add some defines for MIN/MAX macros

--- util.h
+++ util.h
@@ -1,7 +1,12 @@
 /* See LICENSE file for copyright and license details. */
+#ifndef MAX
+  #define MAX(A, B)               ((A) > (B) ? (A) : (B))
+#endif
 
-#define MAX(A, B)               ((A) > (B) ? (A) : (B))
-#define MIN(A, B)               ((A) < (B) ? (A) : (B))
+#ifndef MIN
+  #define MIN(A, B)               ((A) < (B) ? (A) : (B))
+#endif
+
 #define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
 
 #define ISNULL(variable)  (variable == NULL)
