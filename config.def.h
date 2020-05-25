0748e053741fb3b2d9557ef78197c808e5ee1639
Marco Caimi <caimi.marco@protonmail.ch>
2020-05-25 16:08:15 +0200

    Added CenteredMaster layout

--- config.def.h
+++ config.def.h
@@ -69,6 +69,8 @@ static const Layout layouts[] = {
   { "[M]",      monocle },
   { "TTT",      bstack },
   { "===",      bstackhoriz },
+  { "|M|",      centeredmaster},
+  { ">M>",      centeredfloatingmaster},
 };
 
 /* key definitions */
@@ -94,7 +96,7 @@ static Key keys[] = {
   { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
   { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
   { MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
-  { MODKEY,                       XK_b,      togglebar,      {0} },
+  { MODKEY|ControlMask,           XK_b,      togglebar,      {0} },
   { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
   { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
   { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -110,8 +112,10 @@ static Key keys[] = {
   { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
   { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
   { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-  { MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
-  { MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
+  { MODKEY,                       XK_b,      setlayout,      {.v = &layouts[3]} },
+  { MODKEY,                       XK_n,      setlayout,      {.v = &layouts[4]} },
+  { MODKEY,                       XK_c,      setlayout,      {.v = &layouts[5]} },
+  { MODKEY,                       XK_v,      setlayout,      {.v = &layouts[6]} },
   { MODKEY,                       XK_space,  setlayout,      {0} },
   { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
   { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
