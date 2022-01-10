b2e3a3126a9c1b1d1201af3fc2bb1a15e20729f4
Marco Caimi <caimi.marco@protonmail.ch>
2022-01-10 14:57:56 +0100

    Upstream Patch: drawbar: Don't expend effort drawing bar if it is occluded
    I noticed that a non-trivial amount of dwm's work on my machine was from
    drw_text, which seemed weird, because I have the bar disabled and we
    only use drw_text as part of bar drawing.
    
    Looking more closely, I realised that while we use m->showbar when
    updating the monitor bar margins, but don't skip actually drawing the
    bar if it is hidden. This patch skips drawing it entirely if that is the
    case.
    
    On my machine, this takes 10% of dwm's on-CPU time, primarily from
    restack() and focus().
    
    When the bar is toggled on again, the X server will generate an Expose
    event, and we'll redraw the bar as normal as part of expose().

--- dwm.c
+++ dwm.c
@@ -561,6 +561,9 @@ drawbar(Monitor *m)
   unsigned int i, occ = 0, urg = 0, middle;
   Client *c;
 
+  if (!m->showbar)
+    return;
+
   if(showsystray && m == systraytomon(m))
     stw = getsystraywidth();
 
