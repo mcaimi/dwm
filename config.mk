c3ceb4c585c408964c3e931f78e0e0a94e90c69e
Marco Caimi <mcaimi@redhat.com>
2023-03-09 09:28:02 +0100

    Upstream Patch: config.mk: update to _XOPEN_SOURCE=700L
    SA_NOCLDWAIT is marked as XSI in the posix spec [0] and FreeBSD and NetBSD
    seems to more be strict about the feature test macro [1].
    
    so update the macro to use _XOPEN_SOURCE=700L instead, which is equivalent to
    _POSIX_C_SOURCE=200809L except that it also unlocks the X/Open System
    Interfaces.
    
    [0]: https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/signal.h.html#tag_13_42
    [1]: https://lists.suckless.org/dev/2302/35111.html
    
    Tested on:
    * NetBSD 9.3 (fixed).
    * FreeBSD 13 (fixed).
    * Void Linux musl.
    * Void Linux glibc.
    * OpenBSD 7.2 (stable).
    * Slackware 11.
    
    Reported-by: beastie <pufferfish@riseup.net>

--- config.mk
+++ config.mk
@@ -31,7 +31,7 @@ INCS = -I${X11INC} -I${FREETYPEINC} `pkg-config --cflags xft pango pangoxft`
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${XSHAPELIBS} ${FREETYPELIBS} -lXrender `pkg-config --libs xft pango pangoxft`
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} ${XSHAPEFLAGS}
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} ${XSHAPEFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
